a2a0da9430ce47e3734ce0beb4d448f90c8065f2
liuchengshuai001 <liuchengshuai001@ke.com>
2021-02-06 23:08:58 +0800

    feat: 新增 service worker

---
+++ serviceworker.js
@@ -0,0 +1,20 @@
+"use strict";
+self.addEventListener('fetch', function (event) {
+  console.log(event.request.url)
+  if (/(\.jpe?g|\.png)$/.test(event.request.url)) {
+    var supportsWebp = false;
+    if (event.request.headers.has('accept')) {
+      supportsWebp = event.request.headers.get('accept').includes('webp');
+    }
+    if (supportsWebp) {
+      var req = event.request.clone();
+      console.log(req.url)
+      var returnUrl = req.url + "?imageView2/0/format/webp/q/75";
+      event.respondWith(
+        fetch(returnUrl, {
+          mode: 'no-cors'
+        })
+      );
+    }
+  }
+});
